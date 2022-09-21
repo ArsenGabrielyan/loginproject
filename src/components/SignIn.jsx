e5876a01b7ccb6231ea7f45ff728f1af585b6b79
ArsenGabrielyan <arsentech5002@gmail.com>
2022-09-21 15:14:52 +0400

    Converted components from class to functional + added functionalities (Not Completed)

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,19 +4,12 @@ import { Link } from "react-router-dom";
 import SignInFormComp from "./SignInForm";
 
 function SignInPage(){
-     const [loginStatus, setLoginStatus] = React.useState({});
-     function onSubmitLogin(res){
-          setLoginStatus(res)
-     }
-     React.useEffect(()=>{
-          console.log(loginStatus)
-     }, [loginStatus])
      return(
           <>
           <div className="bg2"></div>
                 <div className="loginContainer">
                 <h2>Sign In</h2>
-               <SignInFormComp onEmailSubmit={onSubmitLogin} onPassSubmit={onSubmitLogin}/>
+               <SignInFormComp />
                <div className="linkPos">
                <Link to="/signup" className="link">Sign Up <Icon icon="akar-icons:arrow-right"/></Link>
           </div>
--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,54 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
+import {useDispatch, useSelector} from "react-redux"
+import { Navigate } from "react-router-dom";
+import {login} from "../actions/auth"
 
-class SignInFormComp extends React.Component{
-     constructor(props){
-          super(props)
-          this.state = {
-               email:"", password: "", isValidEmail: false, isValidPassword: false, message: "", message2: "",
-          };
-          this.handleChangeEmail = this.handleChangeEmail.bind(this)
-          this.handleChangePass = this.handleChangePass.bind(this)
+function SignInFormComp(props){
+     const [email, setEmail] = useState("")
+     const [password, setPassword] = useState("")
+     const [isValidEmail, setIsValidEmail] = useState(false)
+     const [isValidPassword, setIsValidPassword] = useState(false)
+     const [message1, setMessage1] = useState("")
+     const [message2, setMessage2] = useState("")
+
+     const {isLoggedIn} = useSelector(state => state.auth)
+
+     const {message} = useSelector(state => state.message)
+
+     const dispatch = useDispatch()
+
+     function handleChangeEmail(e){
+          setEmail(e.target.value)
      }
-     handleChangeEmail(e){this.setState({email: e.target.value,})}
-     handleChangePass(e){this.setState({password: e.target.value}) }
-     emailValidation(){
+     function handleChangePass(e){
+          setPassword(e.target.value)
+     }
+     function emailValidation(){
           const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i; //eslint-disable-line
-          return !(!this.state.email || regex.test(this.state.email) === false);
+          return !(!email || regex.test(email) !== true);
      }
-     passwordValidation(){return !(!this.state.password || this.state.password.length <= 8)}
-     handleSubmitLogin(){
-          const isPassValid = this.passwordValidation();
-          const isEmailValid = this.emailValidation();
-          this.setState({
-               isValidPassword: isPassValid,
-               message2: isPassValid ? "" : "Invalid Password"
-          },()=> this.props.onPassSubmit(this.state))
-          this.setState({
-               isValidEmail: isEmailValid,
-               message: isEmailValid ? "" : "Invalid Email"
-          },()=> this.props.onEmailSubmit(this.state))
-          if(this.state.isValidEmail && this.state.isValidPassword){
-               console.log(this.state)
+     function passwordValidation(){return !(!password || password.length <= 8)}
+     function handleSubmitLogin(){
+          const isPassValid = passwordValidation();
+          const isEmailValid = emailValidation();
+          setIsValidPassword(isPassValid)
+          setMessage2(isPassValid ? "" : "Invalid Password")
+          setIsValidEmail(isEmailValid)
+          setMessage1(isEmailValid ? "" : "Invalid Email")
+          if(isValidEmail && isValidPassword){
+               dispatch(login(email, password))
+               .then(()=>{
+                    props.history.push("/profile");
+                    window.location.reload();
+               })
+               console.log(email, password)
+          } 
+          if(isLoggedIn){
+               return <Navigate to="/profile" />
           }
      }
-     handlePreventDef(e){
+     function handlePreventDef(e){
           e.preventDefault()
      }
-     render(){
-          return(
-               <>
-               <form className="frm" onSubmit={this.handlePreventDef}>
-               <p className="validError">{this.state.message}</p>
+     return(
+          <>
+               <form className="frm" onSubmit={handlePreventDef}>
+               <p className="validError">{message1}</p>
                <label htmlFor="email" className="labelBox">Email</label>
-                    <input type="email" name="email"  id="email" placeholder="Email address..." className="inputBox" onChange={this.handleChangeEmail} value={this.state.email} />
-                    <p className="validError">{this.state.message2}</p>
+                    <input type="email" name="email"  id="email" placeholder="Email address..." className="inputBox" onChange={handleChangeEmail} value={email} />
+                    <p className="validError">{message2}</p>
                     <label htmlFor="pass" className="labelBox">Password</label>
-                    <input type="password" name="pass"  id="password" placeholder="****************" className="inputBox" onChange={this.handleChangePass} value={this.state.password} />
-                    <button type="submit" className="loginBtn" onClick={() => {this.handleSubmitLogin()}}>Sign Up</button>
+                    <input type="password" name="pass"  id="password" placeholder="****************" className="inputBox" onChange={handleChangePass} value={password} />
+                    <button type="submit" className="loginBtn" onClick={() => handleSubmitLogin()}>Sign In</button>
                </form>
+               {message && (
+                    <p className="validError">{message}</p>
+               )}
                </>
-          )
-     }
+     )
 }
 
 export default SignInFormComp
