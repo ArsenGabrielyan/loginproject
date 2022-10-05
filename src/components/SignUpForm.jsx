203f55c7f0d7c3e5181d4baffc8a939ee64243fe
ArsenGabrielyan <arsentech5002@gmail.com>
2022-10-05 16:14:55 +0400

    Made Code Optimized

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,66 +1,69 @@
 import { Icon } from "@iconify/react";
 import React, {useState, useRef} from "react";
-import {register} from "../actions/auth";
+import AuthService from "../services/authService";
 
 function SignUpFormComp(){
-     const [name, setName] = useState("")
-     const [email, setEmail] = useState("")
-     const [username, setUsername] = useState("")
-     const [pass, setPass] = useState("")
-     const [confirmPass, setConfirmPass] = useState("")
-     const [agreed, setAgreed] = useState(false)
-     const [birthDate, setBirthDate] = useState("")
-     const [phone, setPhone] = useState("")
-     const [selectedFile, setSelectedFile] = useState("")
-
-     const [isValidName, setIsValidName] = useState(false)
-     const [isValidEmail, setIsValidEmail] = useState(false)
-     const [isValidUsername, setIsValidUsername] = useState(false)
-     const [isValidPass, setIsValidPass] = useState(false)
-     const [isPassConfirmed, setIsPassConfirmed] = useState(false)
-     const [isCheckboxChecked, setIsCheckboxChecked] = useState(false)
-     const [isValidDate, setIsValidDate] = useState(false)
-     const [isValidPhone, setIsValidPhone] = useState(false)
-     const [isValidFile, setIsValidFile] = useState(false)
-
-     const [msgName, setMsgName] = useState("")
-     const [msgEmail, setMsgEmail] = useState("")
-     const [msgUsername, setMsgUsername] = useState("")
-     const [msgPass, setMsgPass] = useState("")
-     const [msgConfirmPass, setMsgConfirmPass] = useState("")
-     const [msgCheckbox, setMsgCheckbox] = useState("")
-     const [message, setMessage] = useState("")
-     const [msgDate, setMsgDate] = useState("")
-     const [msgPhone, setMsgPhone] = useState("")
-     const [msgFile, setMsgFile] = useState("")
-
+     const [formData, setFormData] = useState({
+          name: "",
+          email: "",
+          username: "",
+          pass: "",
+          confirmPass: "",
+          agreed: false,
+          birthDate: "",
+          phone: "",
+          selectedFile: ""
+     })
+     const [isValid, setIsValid] = useState({
+          isValidName: false,
+          isValidEmail: false,
+          isValidUsername: false,
+          isValidPass: false,
+          isPassConfirmed: false,
+          isCheckboxChecked: false,
+          isValidDate: false,
+          isValidPhone: false,
+          isValidFile: false
+     })
+     const [msg, setMsg] = useState({
+          msgName: "",
+          msgEmail: "",
+          msgUsername: "",
+          msgPass: "",
+          msgConfirmPass: "",
+          msgCheckbox: "",
+          msgDate: "",
+          msgPhone: "",
+          msgFile: ""
+     })
      const [successful, setSuccessful] = useState(false)
 
      const fileInput = useRef(null)
 
      function resetForm(){
-          setName("")
-          setEmail("")
-          setUsername("")
-          setPass("")
-          setConfirmPass("")
-          setAgreed("")
-          setBirthDate("")
-          setPhone("")
-          setSelectedFile("")
+          let resetValues = {
+               name: "",
+               email: "",
+               username: "",
+               pass: "",
+               confirmPass: "",
+               agreed: false,
+               birthDate: "",
+               phone: "",
+               selectedFile: ""
+          }
+          setFormData(formData => ({
+               ...formData,
+               ...resetValues
+          }))
      }
-     function handleChangeName(e){setName(e.target.value)}
-     function handleChangeEmail(e){setEmail(e.target.value)}
-     function handleChangeUsername(e){setUsername(e.target.value)}
-     function handleChangePass(e){setPass(e.target.value)}
-     function handleChangeConfirmPass(e){setConfirmPass(e.target.value)}
-     function handleChangeCheckbox(e){setAgreed(e.target.checked)}
-     function handleChangePhone(e){setPhone(e.target.value)}
-     function handleChangeDate(e){setBirthDate(e.target.value)}
+     function handleChange(e){
+          setFormData({...formData, [e.target.name]: e.target.value})
+     }
      async function handleChangeFile(){
           const files = fileInput.current?.files[0]
-          const base64 = await convertFilesIntoBase64(files)
-          setSelectedFile(base64)
+          const base64 = await convertFilesIntoBase64(files);
+          setFormData({...formData, selectedFile: base64})
      }
      function convertFilesIntoBase64(file){
           return new Promise((resolve, reject)=>{
@@ -74,19 +77,19 @@ function SignUpFormComp(){
                }
           })
      }
-     function nameValid(){return !(!name || name.length <= 2)}
-     function usernameValid(){return !(!username || username.length <= 2)}
-     function telValid(){return !(!phone || phone.length <= 2)}
-     function dateValid(){const regex =  /^\d{4}-\d{2}-\d{2}$/;return !(!birthDate || !regex.test(birthDate))} // eslint-disable-next-line
-     function emailValid(){const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i; return !(!email || regex.test(email) === false);} // eslint-disable-next-line
-     function passValid(){return !(!pass || pass.length <= 8)}
-     function clickUpload(){document.getElementById("uploadFile").click()}
+     function nameValid(){return !(!formData.name || formData.name.length <= 2)}
+     function usernameValid(){return !(!formData.username || formData.username.length <= 2)}
+     function telValid(){return !(!formData.phone || formData.phone.length <= 2)}
+     function dateValid(){const regex =  /^\d{4}-\d{2}-\d{2}$/;return !(!formData.birthDate || !regex.test(formData.birthDate))} // eslint-disable-next-line
+     function emailValid(){const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i; return !(!formData.email || regex.test(formData.email) === false);} // eslint-disable-next-line
+     function passValid(){return !(!formData.pass || formData.pass.length <= 8)}
+     function clickUpload(){fileInput.current.click()}
      function fileValid(){
           const regex =  /(\.jpg|\.jpeg|\.png|\.gif|\.webp)$/i
-          return !(!selectedFile || regex.exec(selectedFile))
+          return !(!formData.selectedFile || regex.exec(formData.selectedFile))
      }
-     function confirmPassValid(){return !(!confirmPass || pass !== confirmPass)}
-     function checkBoxValid(){return !(!agreed)}
+     function confirmPassValid(){return !(!formData.confirmPass || formData.pass !== formData.confirmPass)}
+     function checkBoxValid(){return !(!formData.agreed)}
      function handleSubmitSignUp(e){
           e.preventDefault()
           setSuccessful(false)
@@ -99,28 +102,30 @@ function SignUpFormComp(){
           const isDateValid = dateValid()
           const isPhoneValid = telValid()
           const isFileValid = fileValid()
-
-          setIsValidFile(isFileValid)
-          setMsgFile(isFileValid ? "" : "Invalid File")
-          setIsValidName(isNameValid)
-          setMsgName(isNameValid ? "" : "Invalid Name")
-          setIsValidEmail(isEmailValid)
-          setMsgEmail(isEmailValid ? "" : "Invalid Email")
-          setIsValidUsername(isUsernameValid)
-          setMsgUsername(isUsernameValid ? "" : "Invalid Username")
-          setIsValidPass(isPasswordValid)
-          setMsgPass(isPasswordValid ? "" : "Invalid Password")
-          setIsPassConfirmed(isConfirmPassValid)
-          setMsgConfirmPass(isConfirmPassValid ? "" : "Password Doesn't Match")
-          setIsCheckboxChecked(isCheckBoxValid)
-          setMsgCheckbox(isCheckBoxValid ? "" : "You Need To Accept the Terms")
-          setIsValidPhone(isPhoneValid)
-          setMsgPhone(isPhoneValid ? "": "Invalid Phone Number")
-          setIsValidDate(isDateValid)
-          setMsgDate(isDateValid ? "" : "Invalid Date")
-
-          if(isValidFile && isValidName && isValidEmail && isValidUsername && isValidDate && isValidPhone && isValidPass && isPassConfirmed && isCheckboxChecked){
-               register(selectedFile,name,email,username,birthDate,phone,pass,confirmPass,agreed)
+          setIsValid({
+               isValidName: isNameValid,
+               isValidEmail: isEmailValid,
+               isValidUsername: isUsernameValid,
+               isValidPass: isPasswordValid,
+               isPassConfirmed: isConfirmPassValid,
+               isCheckboxChecked: isCheckBoxValid,
+               isValidDate: isDateValid,
+               isValidPhone: isPhoneValid,
+               isValidFile: isFileValid
+          })
+          setMsg({
+               msgName: isNameValid ? "" : "Invalid Name",
+               msgEmail: isEmailValid ? "" : "Invalid Email",
+               msgUsername: isUsernameValid ? "" : "Invalid Username",
+               msgPass: isPasswordValid ? "" : "Invalid Password",
+               msgConfirmPass: isConfirmPassValid ? "" : "Password Doesn't Match",
+               msgCheckbox: isCheckBoxValid ? "" : "You Need To Accept the Terms",
+               msgDate: isDateValid ? "" : "Invalid Date",
+               msgPhone: isPhoneValid ? "": "Invalid Phone Number",
+               msgFile: isFileValid ? "" : "Invalid File"
+          })
+          if(isValid.isValidFile && isValid.isValidName && isValid.isValidEmail && isValid.isValidUsername && isValid.isValidDate && isValid.isValidPhone && isValid.isValidPass && isValid.isPassConfirmed && isValid.isCheckboxChecked){
+               AuthService.register(formData.selectedFile,formData.name,formData.email,formData.username,formData.birthDate,formData.phone,formData.pass,formData.confirmPass,formData.agreed)
                e.target.reset()
                resetForm()
           }
@@ -130,7 +135,7 @@ function SignUpFormComp(){
            <form className="frm" onSubmit={handleSubmitSignUp}>
                {!successful && (
                     <>
-                    <p className="validError">{msgFile}</p>
+                    <p className="validError">{msg.msgFile}</p>
                     <label htmlFor="uploadFile" className="labelBox">Profile Picture</label>
                     <button className="uploadBtn" onClick={clickUpload.bind(this)} type="button">
                          <Icon icon="fa-solid:upload" /> Upload
@@ -138,43 +143,40 @@ function SignUpFormComp(){
                     <input type="file" hidden name="file" id="uploadFile" className="inputBox" onChange={e => handleChangeFile(e)} ref={fileInput}/>
                     <label className="labelBox">Preview</label>
                     <br/>
-                    <img src={selectedFile} width="200" />
+                    <img src={formData.selectedFile} alt="Insert to Show" width="200" />
                     <br/>
-                         <p className="validError">{msgName}</p>
+                         <p className="validError">{msg.msgName}</p>
                <label htmlFor="name" className="labelBox">Full Name</label>
                <input 
                type="text" 
                name="name"  
                id="fullName" placeholder="Name..." className="inputBox" 
-               onChange={handleChangeName} value={name} />
-               <p className="validError">{msgEmail}</p>
+               onChange={handleChange} value={formData.name} />
+               <p className="validError">{msg.msgEmail}</p>
                <label htmlFor="email" className="labelBox">Email</label>
-               <input type="email" name="email"  id="email" placeholder="Email address..." className="inputBox" onChange={handleChangeEmail} value={email} />
-               <p className="validError">{msgUsername}</p>
+               <input type="email" name="email"  id="email" placeholder="Email address..." className="inputBox" onChange={handleChange} value={formData.email} />
+               <p className="validError">{msg.msgUsername}</p>
                <label htmlFor="username" className="labelBox">Username</label>
-               <input type="text" name="username"  id="userName" placeholder="Username..." className="inputBox" onChange={handleChangeUsername} value={username}/>
-               <p className="validError">{msgDate}</p>
-               <label htmlFor="date" className="labelBox">Birth Date</label>
-               <input type="date" name="date" id="date"className="inputBox" onChange={handleChangeDate} value={birthDate}/>
-               <p className="validError">{msgPhone}</p>
+               <input type="text" name="username"  id="userName" placeholder="Username..." className="inputBox" onChange={handleChange} value={formData.username}/>
+               <p className="validError">{msg.msgDate}</p>
+               <label htmlFor="birthDate" className="labelBox">Birth Date</label>
+               <input type="date" name="birthDate" id="date"className="inputBox" onChange={handleChange} value={formData.birthDate}/>
+               <p className="validError">{msg.msgPhone}</p>
                <label htmlFor="phone" className="labelBox">Phone Number</label>
-               <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" name="phone" id="phone" placeholder="Phone Number..." className="inputBox" onChange={handleChangePhone} value={phone}/>
-               <p className="validError">{msgPass}</p>
+               <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" name="phone" id="phone" placeholder="Phone Number..." className="inputBox" onChange={handleChange} value={formData.phone}/>
+               <p className="validError">{msg.msgPass}</p>
                <label htmlFor="pass" className="labelBox">Password</label>
-               <input type="password" name="pass"  id="password" placeholder="****************" className="inputBox" onChange={handleChangePass} value={pass} />
-               <p className="validError">{msgConfirmPass}</p>
+               <input type="password" name="pass"  id="password" placeholder="****************" className="inputBox" onChange={handleChange} value={formData.pass} />
+               <p className="validError">{msg.msgConfirmPass}</p>
                <label htmlFor="confirmPass" className="labelBox">Repeat Password</label>
-               <input type="password" name="confirmPass" id="confirmPassword" placeholder="****************" className="inputBox" onChange={handleChangeConfirmPass} value={confirmPass}/>
+               <input type="password" name="confirmPass" id="confirmPassword" placeholder="****************" className="inputBox" onChange={handleChange} value={formData.confirmPass}/>
                <span className="agreedCheckBox">
-                    <p className="validError">{msgCheckbox}</p>
-                    <input type="checkbox" name="agreed" className="agreeCheck" checked={agreed} onChange={handleChangeCheckbox}/>
+                    <p className="validError">{msg.msgCheckbox}</p>
+                    <input type="checkbox" name="agreed" className="agreeCheck" checked={formData.agreed} onChange={handleChange}/>
                     <label htmlFor="agreed" className="agreeText">I agree to the <span className="midBold">Terms of User</span></label>
                </span>
                <button type="submit" className="loginBtn">Sign Up</button>
                     </>
-               )}
-               {message && (
-                    <p className={successful ? "validSuccess" : "validError"}>{message}</p>
                )}
           </form>
           </>
